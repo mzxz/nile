949e275c5b7396ec59c7f05dceebda8aa4a060e4
mzxz <mohamed.abaker110@gmail.com>
2023-01-21 16:08:47 +0000

    adding template

--- nilestore/views.py
+++ nilestore/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Welcome to Home page')
+    return render(request, 'home.html')
 
